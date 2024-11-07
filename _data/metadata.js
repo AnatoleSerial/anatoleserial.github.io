0457068b04a9fc54c0030b37a087194c1d62a783
AnatoleSerial <ainu.minaro@gmail.com>
2024-11-08 00:00:03 +0900

    Moving to 11ty

---
+++ _data/metadata.js
@@ -0,0 +1,10 @@
+export default {
+	title: "AnatoleSerial.com",
+	url: "https://anatoleserial.com/",
+	language: "en",
+	description: "Regrettable, A Blog.",
+	author: {
+		name: "AnatoleSerial",
+		url: "https://anatoleserial.com/"
+	}
+}
