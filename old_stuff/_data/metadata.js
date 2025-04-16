b0af5533bf58a15647e0be61a3d42f0851c81559
AnatoleSerial <ainu.minaro@gmail.com>
2025-04-17 00:04:42 +0900

    Some backup files with useful stuff to pillage from

---
+++ old_stuff/_data/metadata.js
@@ -0,0 +1,10 @@
+export default {
+	title: "AnatoleSerial.com",
+	url: "https://anatoleserial.com/",
+	language: "en",
+	description: "Regrettably, A Web Site.",
+	author: {
+		name: "AnatoleSerial",
+		url: "https://anatoleserial.com/"
+	}
+}
